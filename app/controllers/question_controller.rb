3a56c4c1cd9997eb1aa45a56c997b537b07b7131
Kelsey Edelstein <kelseyedelstein@gmail.com>
2016-11-24 13:09:46 -0800

    Add basic CSS to index

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -5,3 +5,15 @@
 #edit
 #update
 #destroy
+
+get '/' do 
+	redirect '/questions'
+end
+
+get '/questions' do 
+  @questions = Question.all
+  @timestamps = @questions.to_a.map do |question| 
+    question.created_at.strftime('%a %d %b %Y') 
+  end
+  erb :index
+end
