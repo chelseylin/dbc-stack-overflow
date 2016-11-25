be44a65f206d3b8c72cc2fc8338030ce00f73404
Kelsey Edelstein <email": null>
2016-11-25 14:00:43 -0800

    Attempt to implement voting functionality

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -6,20 +6,36 @@
 #update
 #destroy
 
-get '/' do 
+get '/' do
   redirect '/questions'
 end
 
-get '/questions' do 
+get '/questions' do
   @questions = Question.all
-  @timestamps = @questions.to_a.map do |question| 
-    question.created_at.strftime('%a %d %b %Y') 
+  @timestamps = @questions.to_a.map do |question|
+    question.created_at.strftime('%a %d %b %Y')
   end
   erb :index
 end
 
-get '/questions/:id' do 
+get '/questions/:id' do
   @question = Question.find(params[:id])
   @answers = Answer.where("question_id = '#{params[:id]}'")
-  erb :show_question
+  erb :question_and_answers
+end
+
+post '/questions/:id' do
+  @question = Question.find(params[:id])
+  @user = User.find(session[:user_id])
+  @new_vote = Vote.create(user_id: @user.id, target: @question, value: 1)
+
+  if @new_vote.valid?
+    if request.xhr?
+      return @question.votes.count.to_s
+    else
+      erb :question_and_answers
+    end
+  else
+    status 422
+  end
 end
