ebab4b6d1e3b04cdd3d448cab4910b9100e48e19
wushuchris <chrismendoza8@gmail.com>
2016-11-25 13:50:32 -0800

    Added: Ask a question feature

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -6,20 +6,31 @@
 #update
 #destroy
 
-get '/' do 
+get '/' do
   redirect '/questions'
 end
 
-get '/questions' do 
+get '/questions' do
   @questions = Question.all
-  @timestamps = @questions.to_a.map do |question| 
-    question.created_at.strftime('%a %d %b %Y') 
+  @timestamps = @questions.to_a.map do |question|
+    question.created_at.strftime('%a %d %b %Y')
   end
   erb :index
 end
 
-get '/questions/:id' do 
+get '/questions/new' do
+
+  erb :create_question
+end
+
+get '/questions/:id' do
   @question = Question.find(params[:id])
   @answers = Answer.where("question_id = '#{params[:id]}'")
   erb :show_question
+end
+
+post '/questions/new' do
+  @question = Question.create!(title: params[:title], description: params[:description], user_id: session[:user_id])
+
+  redirect '/questions'
 end
