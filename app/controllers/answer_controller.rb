dc9eda10df425fb8b6ca97f1919d78456537f37f
Kelsey Edelstein <email": null>
2016-11-25 13:52:44 -0800

    add answerability

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -5,3 +5,8 @@
 #edit
 #update
 #destroy
+post "/question/:id/answers" do
+  # userid_who_posedQ = User.find_by(question_id: params[:id]
+  Answer.create!(description: params[:answertext], user_id: session[:user_id], question_id: params[:id])
+  redirect "/questions/#{params[:id]}"
+end
