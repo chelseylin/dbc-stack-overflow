c5bf1a6ec08a6b56a2d8ca102c071af206d62ced
Dimesky <matthew.dimesky@gmail.com>
2016-11-26 21:51:27 -0800

    stubbed question text on main page if it goes over 25 words

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,29 +12,32 @@ user9 = User.create!(user_name: Faker::Internet.user_name, password: "abc123ab")
 
 # Questions
 
-question1 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraphs, user_id: 1)
-question2 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraphs, user_id: 1)
-question3 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraphs, user_id: 2)
-question4 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraphs, user_id: 2)
-question5 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraphs, user_id: 3)
-question6 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraphs, user_id: 3)
+question1 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, user_id: 1)
+question2 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, user_id: 1)
+question3 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, user_id: 2)
+question4 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, user_id: 2)
+question5 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, user_id: 3)
+question6 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, user_id: 3)
+question7 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, user_id: 7)
+question8 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, user_id: 8)
+question9 = Question.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, user_id: 9)
 
 # Answers
 
-answer1 = Answer.create!(description: Faker::Lorem.paragraphs, user_id: 2, question_id: 1, best_answer: 1)
+answer1 = Answer.create!(description: Faker::Lorem.paragraph, user_id: 2, question_id: 1, best_answer: 1)
 answer2 = Answer.create!(description: Faker::Lorem.paragraph, user_id: 3, question_id: 1)
-answer3 = Answer.create!(description: Faker::Lorem.paragraphs, user_id: 2, question_id: 2)
+answer3 = Answer.create!(description: Faker::Lorem.paragraph, user_id: 2, question_id: 2)
 answer4 = Answer.create!(description: Faker::Lorem.paragraph, user_id: 3, question_id: 2)
-answer5 = Answer.create!(description: Faker::Lorem.paragraphs, user_id: 1, question_id: 3)
+answer5 = Answer.create!(description: Faker::Lorem.paragraph, user_id: 1, question_id: 3)
 answer6 = Answer.create!(description: Faker::Lorem.paragraph, user_id: 3, question_id: 3)
-answer7 = Answer.create!(description: Faker::Lorem.paragraphs, user_id: 1, question_id: 4)
+answer7 = Answer.create!(description: Faker::Lorem.paragraph, user_id: 1, question_id: 4)
 answer8 = Answer.create!(description: Faker::Lorem.paragraph, user_id: 2, question_id: 5)
-answer9 = Answer.create!(description: Faker::Lorem.paragraphs, user_id: 2, question_id: 6)
+answer9 = Answer.create!(description: Faker::Lorem.paragraph, user_id: 2, question_id: 6)
 
 # Comments
 
-comment1 = Comment.create!(description: "comment for question", user_id: 2, content: question1)
-comment1 = Comment.create!(description: "comment for answer", user_id: 2, content: answer1)
+comment1 = Comment.create!(description: Faker::Lorem.sentence, user_id: 2, content: question1)
+comment1 = Comment.create!(description: Faker::Lorem.sentence, user_id: 2, content: answer1)
 
 # Votes
 
